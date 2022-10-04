9ee45a7221f8c555b893c83a79ba1045e26dd480
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-10-04 20:38:03 +0300

    bookappointment

--- BookAppoint/BookDocAppointment.js
+++ BookAppoint/BookDocAppointment.js
@@ -1,48 +1,115 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function Form() {
-  const [doc, updateDocInfo] = useState('default');
-  const handleDropDown = (e) => {
-    updateDocInfo(e.target.value);
+  let where, when;
+  const [formData, handleForm] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    doc: 'default',
+    where: '',
+    when: '',
+  });
+  const [inprogress, handleInprogress] = useState(false);
+  const [booked, handleBooking] = useState(false);
+  const handleFormData = (event) => {
+    handleForm({ ...formData, [event.target.name]: event.target.value });
   };
+
+  if (formData.doc !== 'default') {
+    where = (
+      <div>
+        <h4>Where?</h4>
+        <input type="radio" value="gmeet" name="where" onChange={handleFormData} />
+        Google Meet
+        <br /> <input type="radio" value="phone" name="where" onChange={handleFormData} />
+        Phone
+      </div>
+    );
+    when = (
+      <div>
+        <h4>When?</h4>
+        <input type="datetime-local" name="when" value={formData.when} onChange={handleFormData} />
+      </div>
+    );
+  }
+  const handleCancel = () => {
+    handleBooking(false);
+    handleForm({
+      fname: '',
+      lname: '',
+      email: '',
+      doc: 'default',
+      where: '',
+      when: '',
+    });
+  };
+  const handleSubmit = async (e) => {
+    handleInprogress(true);
+    e.preventDefault();
+    //get all form data
+    try {
+      const response = await axios('https://jsonplaceholder.typicode.com/posts', formData);
+      if (response) {
+        //appointment booked
+        handleBooking(true);
+        handleInprogress(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form>
-      <h1>Book a Session</h1>
-      <p>Fill in the form below to book a virtual session with your doctor</p>
-      <h4>Basic Info</h4>
-      <label>
-        First Name: <input></input>
-      </label>
-      <br />
-      <label>
-        Last Name: <input></input>
-      </label>
-      <br />
-      <label>
-        Email: <input></input>
-      </label>
-      <br />
-      <h4>Doctor</h4>
-      <select value={doc} onChange={handleDropDown}>
-        <option value="default">Select your doctor</option>
-        <option value="mel">Dr Melissa Luns</option>
-        <option value="sha">Dr Shawn Mendes</option>
-        <option value="kel">Dr Kelly Halo</option>
-      </select>
-      <br />
-      <button type="submit">Confirm Booking</button>
-    </form>
+    <div>
+      {booked ? (
+        <div>
+          <h1>Appointment Booked Successfully</h1>
+          <button onClick={handleCancel}>Cancel Appointment</button>
+        </div>
+      ) : (
+        <div>
+          <h1>Book a Session</h1>
+          <p>Fill in the form below to book a virtual session with your doctor</p>
+          {inprogress ? (
+            <h2>Scheduling the Appointment.....</h2>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <h4>Basic Info</h4>
+              <label>
+                First Name: <input name="fname" value={formData.fname} onChange={handleFormData}></input>
+              </label>
+              <br />
+              <br />
+              <label>
+                Last Name: <input name="lname" value={formData.lname} onChange={handleFormData}></input>
+              </label>
+              <br />
+              <br />
+              <label>
+                Email: <input name="email" value={formData.email} onChange={handleFormData}></input>
+              </label>
+              <br />
+              <h4>Doctor</h4>
+              <select name="doc" value={formData.doc} onChange={handleFormData}>
+                <option value="default">Select your doctor</option>
+                <option value="mel">Dr Melissa Luns</option>
+                <option value="sha">Dr Shawn Mendes</option>
+                <option value="kel">Dr Kelly Halo</option>
+              </select>
+              <div>{where}</div>
+              <div>{when}</div>
+              <br />
+              <button type="submit">Confirm Booking</button>
+            </form>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
-function DoctorInfo() {}
-function PostCall() {}
-function SuccessCall() {}
-function FailureCall() {}
 function BookAppointment() {
-  //select doc- render when/where
-  //on submit- api call + show banner
-  //on success- show msg & button
-  //on failure- show msg & Try Again button
   return (
     <div>
       <Form />
