90bf37ae671b1bfa3c24746cc7e2d6bef3b1d04c
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-10-26 18:11:22 +0530

    tested out blackbox

--- hello-world/src/TestApp.js
+++ hello-world/src/TestApp.js
@@ -1,1 +1,23 @@
 //react test commit
+//find stock price of Amazon in js?
+let itemPrice;
+
+function getPrice(url) {
+  return new Promise((resolve, reject) => {
+    let price;
+    price = 35; // gets the item's price
+    return resolve(price);
+  });
+}
+
+window.onload = function () {
+  getPrice("testurl")
+    .then((price) => {
+      itemPrice = price;
+      console.log('Item Price', itemPrice);
+    })
+    .catch((error) => {
+      // Handling error
+    });
+}
+
