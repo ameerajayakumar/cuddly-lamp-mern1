164e34a168b87eec0a8ca5453738b81416f0e9a9
Ameera Jayakumar <ameeraj.mec@gmail.com>
2023-01-25 15:14:02 +0530

    redux reducer and store

--- redux/trialredux/src/app/store.js
+++ redux/trialredux/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import fruitReducer from '../features/fruits/fruitSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    fruit: fruitReducer,
+  },
 });
--- redux/trialredux/src/features/fruits/fruitSlice.js
+++ redux/trialredux/src/features/fruits/fruitSlice.js
@@ -1,30 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
+  fruit: 'pineapple',
 };
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const fruitSlice = createSlice({
+  name: 'fruit',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    changeFruitName: (state, action) => {
+      state.fruit = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { changeFruitName } = fruitSlice.actions;
 
-export default counterSlice.reducer;
+export default fruitSlice.reducer;
